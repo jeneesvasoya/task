c5e47d7b29958465de913f19d061316cc3e95c72
jeneesvasoya <109714158+jeneesvasoya@users.noreply.github.com>
2022-07-28 12:33:03 +0530

    task

---
+++ Task/Controllers/TController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.Models;
+using Task.Service;
+
+namespace Task.Controllers
+{
+    public class TController : Controller
+    {
+        // GET: T
+        CompanyService s1 = new CompanyService();
+        taskEntities e1 = new taskEntities();
+
+        void fillstate()
+        {
+            var states = from s in e1.states
+                         select new SelectListItem { Value = s.state_id.ToString(), Text = s.state_name };
+            ViewBag.state = states.ToList();
+
+        }
+        public JsonResult Getcitybystateid(int id)
+        {
+            e1.Configuration.ProxyCreationEnabled = false;
+            var cities = e1.cities.Where(c => c.state_id == id).ToList();
+            return Json(cities, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Create()
+        {
+            fillstate();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee obj)
+        {
+            s1.RegisterEmployee(obj);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Index()
+        {
+            return View(s1.GetEmployees());
+        }
+       
+        public ActionResult Edit(int id)
+        {
+            fillstate();
+            return View(s1.GetEmployeesbyid(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee obj)
+        {
+            s1.UpdateEmployee(obj);
+            return RedirectToAction("Index");
+        }
+       
+        public string Delete(int id)
+        {
+            s1.DeleteEmployee(id);
+            return  "Delete Success";
+        }
+    }
+}
---
+++ Task/Service/CompanyService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using Task.Models;
+
+namespace Task.Service
+{
+    public class CompanyService
+    {
+
+        SqlConnection cn;
+        public CompanyService()
+        {
+            cn = new SqlConnection(@"Data Source=DESKTOP-ERKGNQF\MSSQLSERVER01;Initial Catalog=task;Integrated Security=True");
+        }
+        public List<Employee> GetEmployees()
+        {
+            
+            SqlDataAdapter da = new SqlDataAdapter("GetEmployeeList 0,0", cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            List<Employee> li = new List<Employee>();
+            foreach (DataRow item in dt.Rows)
+            {
+                li.Add(new Employee
+                {
+                    Name = item["Name"].ToString(),
+                    Email = item["Email"].ToString(),
+                    Phone_No = Convert.ToInt64(item["Phone_No"].ToString()),
+                    Address = item["Address"].ToString(),
+                    state_id = Convert.ToInt32(item["state_id"].ToString()),
+                    Emp_id = Convert.ToInt32(item["Emp_id"].ToString()),
+                    city_id = Convert.ToInt32(item["city_id"].ToString())
+
+                });
+            }
+            return li;
+        }
+        public Employee GetEmployeesbyid(int id)
+        {
+            
+            SqlDataAdapter da = new SqlDataAdapter($"GetEmployeeList {id},1", cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            Employee obj = new Employee();
+
+            if (dt.Rows.Count>0)
+            {   
+                obj.Name = dt.Rows[0]["Name"].ToString();
+                obj.Email = dt.Rows[0]["Email"].ToString();
+                obj.Phone_No = Convert.ToInt64(dt.Rows[0]["Phone_No"].ToString());
+                obj.Emp_id = Convert.ToInt32(dt.Rows[0]["Emp_id"].ToString());
+                obj.Address = dt.Rows[0]["Address"].ToString();
+                obj.state_id = Convert.ToInt32(dt.Rows[0]["state_id"].ToString());
+                obj.city_id = Convert.ToInt32(dt.Rows[0]["city_id"].ToString());
+            }
+            return obj;
+        }
+
+        public void RegisterEmployee(Employee obj)
+        {
+            
+
+            SqlCommand cmd = new SqlCommand("exec Addemployee @n,@em,@pn,@ad,@st,@ci", cn);
+            cmd.Parameters.AddWithValue("@n", obj.Name);
+            cmd.Parameters.AddWithValue("@em", obj.Email);
+            cmd.Parameters.AddWithValue("@pn", obj.Phone_No);
+            cmd.Parameters.AddWithValue("@ad", obj.Address);
+            cmd.Parameters.AddWithValue("@st", obj.state_id);
+            cmd.Parameters.AddWithValue("@ci", obj.city_id);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+
+        }
+        public void UpdateEmployee(Employee obj)
+        {
+            
+            SqlCommand cmd = new SqlCommand("exec Editemployee @n,@em,@pn,@ad,@st,@ci,@id", cn);
+            cmd.Parameters.AddWithValue("@n", obj.Name);
+            cmd.Parameters.AddWithValue("@em", obj.Email);
+            cmd.Parameters.AddWithValue("@pn", obj.Phone_No);
+            cmd.Parameters.AddWithValue("@id", obj.Emp_id);
+            cmd.Parameters.AddWithValue("@ad", obj.Address);
+            cmd.Parameters.AddWithValue("@st", obj.state_id);
+            cmd.Parameters.AddWithValue("@ci", obj.city_id);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+
+        }
+        public void DeleteEmployee(int id)
+        {
+            
+            SqlCommand cmd = new SqlCommand("exec DeleteEmployee @id", cn);
+            cmd.Parameters.AddWithValue("@id", id);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+
+        }
+    }
+}
